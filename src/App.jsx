81a4e4f83b564c6f0e397320191653244c647e43
Yash Dhiman <yashdhiman745@gmail.com>
2024-10-05 20:25:45 +0530

    commited success

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Edit from './Components/Edit'
 import Bar from './Components/Bar'
 import Signup from './Components/Signup'
 import "./App.css";
